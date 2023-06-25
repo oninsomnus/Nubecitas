241b0e717c2804e01d764da65e228f97fa81f2f2
feriodismo <feriodismo@gmail.com>
2023-06-25 17:56:37 -0400

    FB-4 - Adding test framework

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = 3500;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
 });
 
 app.listen(port, () => {
