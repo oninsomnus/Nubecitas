81a09715db91eebe4f05710640f21dce5f3423cc
Juan Diego <jdcm10@gmail.com>
2023-06-25 16:38:27 -0500

    changes on logs and websocket
    Co-authored-by: fabio bonfanti <feriodismo@users.noreply.github.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,8 @@ import path from 'path';
 import ws from 'ws';
 import fs from 'fs';
 
-const PORT = 3000;
-const WS_PORT = 3500;
+const PORT = 3500;
+const WS_PORT = 3000;
 const LOG_FOLDER = path.join(__dirname, "../", "/logs")
 const LOG_FILE = path.join(LOG_FOLDER, "/texts.log");
 
@@ -13,7 +13,7 @@ const wss = new ws.Server({ port: WS_PORT });
 
 app.get('/', (req, res) => {
     if(req.query.logger) addLineToFile(`[http]:${req.query.logger.toString()}`);
-    res.send('Helslo World!');
+    res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
@@ -21,19 +21,21 @@ app.listen(PORT, () => {
 });
 
 wss.on('connection', (ws: any) => {
-    
-    ws.on('camera', (data: any) => {
+    addLineToFile(`[ws]:Conexion establecida`);
 
-    });
-
-    ws.on('text', (data: string) => {
+    ws.on('message', (data: any) => {
         addLineToFile(`[ws]:${data}`);
     });
+
+    ws.on('close', () => {
+        addLineToFile(`[ws]:Desconectado`);
+    })
 });
 
 const addLineToFile = (text: string): void => {
     if(!fs.existsSync(LOG_FOLDER)) createLogFolder();
     if(!fs.existsSync(LOG_FILE)) createLogFile();
+    console.log(parseText(text));
     fs.appendFileSync(LOG_FILE, parseText(text));
 }
 
