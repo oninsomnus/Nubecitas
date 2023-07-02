6cece82d6e6d13a821ce599145e6f3b0a153874b
Juan Diego <jdcm10@gmail.com>
2023-07-02 17:24:58 -0500

    Test cases and channels implementation
    Co-authored-by: fabio bonfanti <feriodismo@users.noreply.github.com>

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -14,3 +14,12 @@ export const HTTP_PATHS = {
     ROOT: "/",
     TEST: "/ping"
 }
+
+const wrongMessage = (channel: string) => {
+    return ` This channel ${channel} doesn't exist`;
+}
+
+export const ERRORS = {
+    WRONG_WS_TYPE: ' The WebSocket message type is not { channel: string, data: string }',
+    WRONG_CHANNEL: wrongMessage
+}
